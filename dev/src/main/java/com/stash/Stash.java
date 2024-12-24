49db9385fd3fa0a6dc260ac47a3e5e692c6fd4c8
mubin <mubinsayyed19@gmail.com>
2024-12-24 19:28:36 +0530

    Imp work

---
+++ dev/src/main/java/com/stash/Stash.java
@@ -0,0 +1,21 @@
+package com.stash;
+
+public class Stash {
+	
+		
+		public void add(int a, int b) {
+			System.out.println(a+b);
+		}
+		
+		public int sub(int a, int b) {
+			return a+b;
+		
+
+		}
+	
+	
+
+}
+
+
+
